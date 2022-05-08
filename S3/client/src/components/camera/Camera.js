f34db06e749d42881371132b37250e20a9220f74
Miguel Ángel Picazo Fernández <miguelangelbs99@gmail.com>
2022-05-08 18:24:43 +0200

    IU camera v1

--- S3/client/src/components/camera/Camera.js
+++ S3/client/src/components/camera/Camera.js
@@ -1,14 +1,36 @@
-import { logicCamera } from '../camera/logicCamera';
+//import { createRoot } from 'react-dom/client';
+import Webcam from "react-webcam";
+import React from "react";
+
+//const container = document.getElementsByTagName('video');
+//const root = createRoot(container);
 
 function Camera({className,client}){
-    return(
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+    
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+    }, [webcamRef, setImgSrc]);
+
+    return (
         <>
-            <video id="video"></video>
-            <button id="startbutton" onClick={() => {logicCamera()}}>Take photo</button>
-            <canvas id="canvas"></canvas>
-            <img src="http://placekitten.com/g/320/261" id="photo" alt="photo"></img>
+            <Webcam 
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            />
+            <button onClick={capture}>Capture photo</button>
+            {imgSrc && (
+            <img
+                src={imgSrc}
+            />
+            )}
         </>
-    )
-}
+        );
+};
+
+        //root.render(<WebcamCapture />);
 
 export default Camera;
